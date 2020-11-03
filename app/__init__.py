34c9189151f25eb0e81f20638396043003e5b98c
Ernest <248113004@qq.com>
2020-11-03 17:37:02 +0800

    表单，数据库，用户登录

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from config import Config#从config模块导入Config类
 
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+login = LoginManager(app)
+login.login_view='login'
 
 print('等会谁（哪个包或模块）在使用我：',__name__)
 
-from app import routes
+from app import routes,models
 
 
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from app import app
-from flask import  render_template
+from app.forms import LoginForm
+from flask import render_template,flash,redirect,url_for,request
+
+from flask_login import current_user,login_user,logout_user,login_required
+from app.models import User
+
+from werkzeug.urls import url_parse
 
 #2个路由
 @app.route('/')
 @app.route('/index')
-#1个视图函数
+@login_required
 def index():
-	user = {'username': 'Miguel'}  # 用户
 	posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
 		{
 			'author': {'username': 'John'},
@@ -17,4 +22,27 @@ def index():
 			'body': 'The Avengers movie was so cool!'
 		}
 	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home',  posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = LoginForm()#表单实例化对象
+	if form.validate_on_submit():
+		user = User.query.filter_by(username = form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user,remember=form.remember_me.data)
+
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc !='':
+			next_page = url_for('index')
+		return redirect(next_page)
+	return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
